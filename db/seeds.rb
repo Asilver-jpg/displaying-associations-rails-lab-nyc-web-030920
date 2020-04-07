95c4ba870c7afe92ac772e7a8c4b8ec10bf4c1b2
Asilver-jpg <asilver7493@gmail.com>
2020-04-07 13:40:47 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+a=Artist.create(name:"Rage Against the Machine")
+b=Artist.create(name:"Carpenter Brut")
+c=Artist.create(name:"Mick Gordon")
+
+Song.create(title:"Killing in the name of", artist_id: a.id)
+Song.create(title:"Roller Mobster", artist_id: b.id)
+Song.create(title:"BFG Division", artist_id: c.id)
